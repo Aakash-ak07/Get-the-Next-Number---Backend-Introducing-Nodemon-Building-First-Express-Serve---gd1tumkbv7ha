5134c838890b25bbac26bf68fae6d1ed3e2088cb
Aakash Manihar <111000108+Aakash-ak07@users.noreply.github.com>
2023-04-18 23:39:25 +0530

    commit-node

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 
-//Middlewares
+// Middlewares
 app.use(express.json());
 
-// Write a GET Request to get the next number from the input 'num'.
-// Endpoint : /api/get-next-num
-// Return the response as {message : , status: }
+// GET Request to get the next number from the input 'num'.
+// Endpoint: /api/get-next-num
+// Return the response as { message: , status: }
+
+app.get('/api/get-next-num', (req, res) => {
+    const num = parseInt(req.body.num);
+
+    // Check if num is a valid number
+    if (isNaN(num)) {
+        res.status(400).json({ message: "Invalid number", status: "failure" });
+    } 
+    else {
+        const nextNum = num + 1;
+        res.json({ message: nextNum, status: "success" });
+    }
+});
 
 module.exports = app;
